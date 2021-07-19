09d2fc12db03855aaac80507d55df6b85161579a
MicRym <85758026+MicRym@users.noreply.github.com>
2021-07-19 23:07:44 +0200

    Prototypes of scripts added

---
+++ Polski_GameJamGra/Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    int HP;
+    [SerializeField]
+    float Speed = 2.5f;
+    [SerializeField]
+    Vector2 Direction=new Vector2(0,0);
+    [SerializeField]
+    Sprite DefaultSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
